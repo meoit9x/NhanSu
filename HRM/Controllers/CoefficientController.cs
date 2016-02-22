c415681dde5c6f5756063dce86e958817101c29f
hung_nguyen <hungnguyen1.bhtech@gmail.com>
2016-02-22 21:08:01 +0700

    no message

---
+++ HRM/Controllers/CoefficientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRM.Controllers
+{
+    public class CoefficientController : Controller
+    {
+        // GET: Coefficient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HRM/Controllers/OrganizationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRM.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
