d6840266f51e1a83aa8d4a6ea3c31cf789c0c239
hung_nguyen <hungnguyen1.bhtech@gmail.com>
2016-02-22 21:27:38 +0700

    no message

--- HRM/Scripts/sb-admin-2.js
+++ HRM/Scripts/sb-admin-2.js
@@ -23,7 +23,6 @@ $(function() {
             $("#page-wrapper").css("min-height", (height) + "px");
         }
     });
-
     var url = window.location;
     var element = $('ul.nav a').filter(function() {
         return this.href == url || url.href.indexOf(this.href) == 0;
