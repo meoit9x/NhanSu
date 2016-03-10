b3cf2784444e869a7c289ad322ece26c09dfc93f
hung_nguyen <hungnguyen1.bhtech@gmail.com>
2016-03-10 22:40:03 +0700

    no message

---
+++ HRM/Controllers/MaterialController.cs
@@ -0,0 +1,26 @@
+﻿using HRM.Data;
+using HRM.FirstReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRM.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string from, string to, string code)
+        {
+            FirstService s = new FirstService();
+            var list = s.GetData(from, to, code).ToList();
+            return Json(new { data = list, Status = true }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
